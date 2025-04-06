1032e6394f38dd39572dfa23a987813cde196e60
Kamala Joshi <kamalajoshi@Kamalas-MacBook-Air.local>
2025-04-06 19:02:01 -0400

    feat(videoCall):component setup for frontend

---
+++ frontend/app/api/lemurRequest/route.ts
@@ -0,0 +1,35 @@
+import { AssemblyAI } from 'assemblyai';
+
+export async function POST(request: Request) {
+  const apiKey = process.env.ASSEMBLY_API_KEY;
+  if (!apiKey) {
+    return Response.error();
+  }
+
+  const client = new AssemblyAI({ apiKey: apiKey });
+  const body = await request.json();
+
+  const prompt = body?.prompt;
+
+  if (!prompt) {
+    return Response.error();
+  }
+
+  const finalPrompt = `You act as an assistant during a video call. You get a question and I want you to answer it directly without repeating it.
+  If you do not know the answer, clearly state that.
+  Here is the user question:
+  ${prompt}`;
+
+  const lemurResponse = await client.lemur.task({
+    prompt: finalPrompt,
+    input_text: 'This is a conversation during a video call.',
+    // TODO: For now we just give some context, but here we could add the actual meeting text.
+  });
+
+  const response = {
+    prompt: prompt,
+    response: lemurResponse.response,
+  };
+
+  return Response.json(response);
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,103 +1,66 @@
-import Image from "next/image";
+'use client';
+
+import CallContainer from '@/components/CallContainer';
+import ErrorScreen from '@/components/ErrorScreen';
+import { User } from '@stream-io/video-client';
+import { useCallback, useEffect, useState } from 'react';
+
+type HomeState = {
+  apiKey: string;
+  user: User;
+  token: string;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [homeState, setHomeState] = useState<HomeState | undefined>();
+  const [error, setError] = useState<string | undefined>();
+  const getUserTokenFunction = useCallback(getUserToken, []);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect(() => {
+    // TODO: replace by login mechanism and real IDs
+    const userId = 'SampleUser_6d798e3c-9cd1-4018-8940-b3bed4816dee';
+    getUserTokenFunction(userId, 'Sample User');
+  }, [getUserTokenFunction]);
+
+  if (error) {
+    return <ErrorScreen error={error} />;
+  }
+
+  if (homeState) {
+    return <CallContainer {...homeState} />;
+  }
+
+  return (
+    <section className='w-screen-h-screen flex items-center justify-center'>
+      <h1 className='animate-pulse'>Loading</h1>
+    </section>
   );
+
+  async function getUserToken(userId: string, userName: string) {
+    const response = await fetch('/api/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userId: userId }),
+    });
+
+    const responseBody = await response.json();
+    const token = responseBody.token;
+
+    if (!token) {
+      setError('No token found');
+    }
+
+    const user: User = {
+      id: userId,
+      name: userName,
+      image: `https://getstream.io/random_png/?id=${userId}&name=${userName}`,
+    };
+
+    const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+    if (apiKey) {
+      setHomeState({ apiKey: apiKey, user: user, token: token });
+    } else {
+      setError('API key not found. Please add to your environment file.');
+    }
+  }
 }
