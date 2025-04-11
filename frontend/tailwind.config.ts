73b714d32ac412588917b3be64f78b5edb407637
Kamala Joshi <kamalajoshi@Kamalas-MacBook-Air.local>
2025-04-11 13:46:31 -0400

    maintain config files

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,13 +1,30 @@
-import { Config } from "tailwindcss";
-const withMT = require("@material-tailwind/react/utils/withMT");
+// import { Config } from "tailwindcss";
+// const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config = withMT({
+// const config = withMT({
+//   content: [
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {},
+//   plugins: [],
+// });
+
+// export default config;
+// tailwind.config.ts
+// tailwind.config.ts
+import { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
-});
+};
 
 export default config;
+
