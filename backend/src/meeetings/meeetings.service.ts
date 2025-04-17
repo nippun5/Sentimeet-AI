ff30c62147d3f566929c9a8ff1a3b49e307ed637
Kamala Joshi <kamalajoshi@Kamalas-MacBook-Air.local>
2025-04-17 02:22:52 -0400

    fix issues

--- backend/src/meeetings/meeetings.service.ts
+++ backend/src/meeetings/meeetings.service.ts
@@ -84,17 +84,20 @@ async analysis(meetingId: string) {
     }
 
     const prompt = geminiPrompt+`Transcript:
+
     ${transcript}
     `.trim();
-
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
     const result = await model.generateContent(prompt);
     const response = await result.response;
     let text = response.text();
+    console.log(text, "text");
 
-    text = text.replace(/⁠ json| ⁠/g, "").trim();
+    // text = text.replace(/⁠ json| ⁠/g, "").trim();
 
+    text = text.replace(/```(?:json)?|```/g, "").trim();
     const parsed = JSON.parse(text);
+    
 
     await this.prisma.meeting.update({
       where: { id: meetingId },
@@ -103,7 +106,7 @@ async analysis(meetingId: string) {
       },
     });
 
-    return parsed;
+    return text;
   } catch (error) {
     console.error("Gemini API Error:", error.message);
     throw new Error("Failed to process the request");
